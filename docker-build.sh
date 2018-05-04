94e89aa03968fe55fda124c99bb695422895aa94
Pavel SHumskii <shumskii@gmail.com>
2018-05-04 07:01:08 +0700

    fix in progress

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 #build main app
-docker build -f docker/Dockerfile-backend -t nod:backend .
+docker build\
+ -f docker/Dockerfile-backend\
+ --build-arg UID=2000\
+ --build-arg GID=2000\
+ -t nod:backend .
